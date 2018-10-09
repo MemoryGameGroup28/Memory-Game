def310e8a24ad56f731f532e5675bb4881bc6efe
Guido Riebeek <guidoriebeek@hotmail.com>
2018-10-09 17:23:38 +0200

    Added game files

---
+++ Memory Game/GameGrid.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Memory_Game
+{
+    public class GameGrid
+    {
+        // Create the Grid
+        private Grid grid;
+        private int ImageRows = 4;
+        private int ImageCols = 4;
+
+
+        public GameGrid(Grid grid, int GridCol, int GridRow)
+        {
+            this.grid = grid;
+            this.ImageCols = ImageCols;
+            this.ImageRows = ImageRows;
+            InitializeMemoryGrid(GridCol, GridRow);
+            AddImages();
+
+        }
+
+        //Generate MemoryGrid with parameters assigned to ImageRows/Cols + GridRow/Col
+        private void InitializeMemoryGrid(int GridCol, int GridRow)
+        {
+            for (int i = 0; i < GridCol; i++)
+            {
+                grid.ColumnDefinitions.Add(new ColumnDefinition());
+
+            }
+            for (int i = 0; i < GridRow; i++)
+            {
+                grid.RowDefinitions.Add(new RowDefinition());
+            }
+        }
+
+        //Add background images to the generated grid
+        private void AddImages()
+        {
+            for(int GridRow = 0; GridRow < ImageRows; GridRow++)
+            {
+                for (int GridCol = 0; GridCol < ImageCols; GridCol++)
+                {
+                    Image backgroundImage = new Image();
+                    backgroundImage.Source = new BitmapImage(new Uri("Files/cardBackground.png", UriKind.Relative)); //Set background image
+                    backgroundImage.MouseDown += new MouseButtonEventHandler(CardClick);
+
+                    Grid.SetColumn(backgroundImage, GridCol);
+                    Grid.SetRow(backgroundImage, GridRow);
+                    grid.Children.Add(backgroundImage);
+
+                }
+            }
+        }
+
+        //On click event when user clicks on a card
+        private void CardClick(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Test");
+        }
+
+
+        //Image Randomizer:
+
+
+
+
+
+        private void AddLabel()
+        {
+            Label title = new Label();
+            title.Content = "Memory";
+            title.FontFamily = new FontFamily("Ariel");
+            title.FontSize = 40;
+            title.HorizontalAlignment = HorizontalAlignment.Center;
+
+            Grid.SetColumn(title, 1);
+            grid.Children.Add(title);
+        }
+
+
+
+    }
+}
---
+++ Memory Game/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Memory_Game
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private int GridRow = 4;
+        private int GridCol = 4;
+
+
+
+        //HOW TO USE A CLASS WITHIN THE MAIN WINDOW: GameGrid gameGrid = new GameGrid(col, row); 
+
+
+        //Main Game Screen that initializes all game components
+        public MainWindow()
+        {
+            InitializeComponent();
+            GameGrid grid = new GameGrid(GameGrid, GridCol, GridRow);
+        }
+
+        
+        //Give a certain width/height to each column/row item within the game
+        private void InitializeGameGrid()
+        {
+            ColumnDefinition gameColumn = new ColumnDefinition();
+            gameColumn.Width = new GridLength(7, GridUnitType.Star);
+            GameGrid.ColumnDefinitions.Add(gameColumn);
+
+            ColumnDefinition scoreColumn = new ColumnDefinition();
+            scoreColumn.Width = new GridLength(3, GridUnitType.Star);
+            GameGrid.ColumnDefinitions.Add(scoreColumn);
+        }
+
+    }
+
+}
+
